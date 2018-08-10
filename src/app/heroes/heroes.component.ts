782cb5bb193a35a89aa976a4eb2a2182c2525e88
antsiferovapi <polina.antsiferova@oxagile.com>
2018-08-10 18:47:16 +0300

    4. Displaying a List (not ready 2)

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,5 @@
-import {Component, OnInit} from '@angular/core';
-import {Hero} from '../hero';
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
 
 @Component({
@@ -9,11 +9,11 @@ import { HEROES } from '../mock-heroes';
 })
 
 export class HeroesComponent implements OnInit {
- /* hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };*/
   heroes = HEROES;
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 
   ngOnInit() {
 
